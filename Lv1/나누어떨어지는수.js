b06d1b8a79ba83a5bcb6da7f6a3cac4785decb2b
최한울/21500761 <21500761@handong.ac.kr>
2022-11-19 22:16:06 +0900

    add 나누어 떨어지는수.js

---
+++ Lv1/나누어떨어지는수.js
@@ -0,0 +1,16 @@
+function solution(arr, divisor) {
+
+  let answer = arr.reduce((acc, cur, idx) => {
+      if (cur % divisor == 0)
+          acc[idx] = cur
+      else 
+          return acc
+      return acc 
+  },[]);
+  answer = answer.filter((element, i) => element !== undefined);
+    
+  return answer.length == 0 ? [-1] : answer.sort((a,b) => {return a - b;});
+}
+console.log(solution([5,9,7,10], 5))
+console.log(solution([2,36,1,3], 1))
+console.log(solution([3,2,6], 10))

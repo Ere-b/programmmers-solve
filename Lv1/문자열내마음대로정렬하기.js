d87fb5f8dab8ab77fb4549966f48585910523ff5
hanul choe <hanul.developer@gmail.com>
2022-11-21 19:33:38 +0900

    문자열내마음대로정렬하기 풀이

---
+++ Lv1/문자열내마음대로정렬하기.js
@@ -0,0 +1,15 @@
+function solution(strings, n) {
+  let arr = strings;
+  // arr = ...strings;
+
+  arr = arr.map(item => [...item]);
+
+  for(let i = 0; i < arr.length; i++) {
+    
+  }
+
+  return arr;
+}
+
+console.log(solution(["sun", "bed", "car"]));
+console.log(solution(["abce", "abcd", "cdx"]));

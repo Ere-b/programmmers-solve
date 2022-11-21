25d8d10f1c2d4aa8a24e20c3a28d93442ea51c97
hanul choe <hanul.developer@gmail.com>
2022-11-21 19:33:12 +0900

    두개뽑아서더하기 풀이

---
+++ Lv1/두개뽑아서더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+  // console.log(numbers);
+  return (numbers = numbers => {
+    console.log(numbers);
+  });
+}
+
+solution([2, 1, 3, 4, 1]);
+solution([5, 0, 2, 7]);
+// console.log(solution([2,1,3,4,1]))
+// console.log(solution([5,0,2,7]));
